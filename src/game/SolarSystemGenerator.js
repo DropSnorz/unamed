3a83195580473429cccac017cbdf0bdfc2bb4f96
Arthur POIRET <dropsnorz@gmail.com>
2020-04-05 18:03:13 +0200

    dev: basic planets generation

--- src/game/SolarSystemGenerator.js
+++ src/game/SolarSystemGenerator.js
@@ -1,4 +1,5 @@
 import RandomGenerator from './RandomGenerator';
+import Utils from './Utils'
 
 class SolarSystemGenerator {
   constructor(seed, constellation) {
@@ -7,15 +8,31 @@ class SolarSystemGenerator {
   }
 
   generate() {
-    return {
+    let solarSystem = {
       name: this.createName(),
-      constellationName: this.constellation.name,
-      constellation: this.constellation
+      constellation: this.constellation,
+      planets: []
     }
+
+    let nPlanets = this.randomValues.randomGaussian(2, 10, 0);
+    for (let i = 0; i < nPlanets; i++) {
+      solarSystem.planets.push(this.createPlanet(i + 1))
+    }
+    return solarSystem
   }
 
   createName() {
     return this.constellation.name + "-" + this.randomValues.random().toString(36).slice(-this.randomValues.random(3, 4, 0))
+  }
+
+  createPlanet(planetIndex) {
+    let planetTypes = ["Barren", "Ice", "Lava", "Oceanic", "Plasma", "Temperate", "Gas"]
+    let typeIndex = this.randomValues.random(0, planetTypes.length, 0);
+    let type = planetTypes[typeIndex]
+    return {
+      name: "Planet " + Utils.romanize(planetIndex) + " (" + type + ")",
+      type: type
+    }
   }
 
 }

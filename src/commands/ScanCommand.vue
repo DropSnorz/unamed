2033c65a222c8aaf4486abec067af07392ffffac
Arthur POIRET <dropsnorz@gmail.com>
2020-04-07 22:37:19 +0200

    dev: scan reachable constellations and systems

--- src/commands/ScanCommand.vue
+++ src/commands/ScanCommand.vue
@@ -4,6 +4,12 @@
     <ul>
       <li v-for="planet in planets" :key="planet.name">{{ planet.name }}</li>
     </ul>
+    <ul>
+      <li v-for="system in systems" :key="system.name">{{ system.name }}</li>
+    </ul>
+    <ul>
+      <li v-for="constellation in constellations" :key="constellation.name">{{ constellation.name }}</li>
+    </ul>
   </div>
 </template>
 
@@ -13,18 +19,34 @@ export default {
   data: function() {
     return {
       planets: [],
+      systems: [],
+      constellations: [],
       systemName: ""
     };
   },
   mounted() {
     this.$nextTick(function() {
-      let planets = [];
       let currentSystem = this.$store.state.player.currentSystem;
       this.systemName = currentSystem.name;
+
+      let planets = [];
       for (let planet of currentSystem.planets) {
         planets.push(planet);
       }
       this.planets = planets;
+
+      let systems = [];
+      for (let system of currentSystem.constellation.systems) {
+        systems.push(system);
+      }
+      this.systems = systems;
+
+      let constellations = [];
+      for (let constellation of currentSystem.constellation.paths) {
+        constellations.push(constellation);
+      }
+      this.constellations = constellations;
+
       this.leave();
     });
   },
